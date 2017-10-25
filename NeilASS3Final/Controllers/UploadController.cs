dfb326b45282f196437e5cff5b86ccf7a6364a1d
gingyneil <neil.reading.nr@gmail.com>
2017-10-25 16:48:15 -0400

    Add project files.

---
+++ NeilASS3Final/Controllers/UploadController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NeilASS3Final.Controllers
+{
+    [Authorize]
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ NeilASS3Final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NeilASS3Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
